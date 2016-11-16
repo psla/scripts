d7ab848f63d19e4dc8f6daa00bca3a6e09437432
Piotr Slatala <piotr@sepio.pl>
2016-11-15 18:17:52 -0800

    update tc.sh with some comments

--- tc.sh
+++ tc.sh
@@ -1,5 +1,7 @@
-infractus:/etc# cat /etc/autoexec/tc.sh
 #!/bin/bash
+
+# throughput / traffic control - politeness rules that always prioritize some protocols over others (on upload).
+# note that this script does not control download at all
 
 #reset qdics
 tc qdisc del dev ${INET} root handle 1: 2>/dev/null
@@ -13,7 +15,6 @@ HIGH_PRIO_RATE=100kbit
 NETWORK_RATE=1770kbit
 SERVER_RATE=100kbit
 
-
 PHONE_CEIL=300kbit
 
 INET=eth0
@@ -26,34 +27,33 @@ tc class add dev ${INET} parent 1: classid 1:1 htb rate ${MAXUP} ceil ${MAXUP}
 
 #traffic control
 
-#tutaj trafi telefon. zawsze pierwszy. niezaleznie od wszystkiego.
-#here comes phone. No matter what, it's always first
+# here comes phone. No matter what, it's always first (as long as we don't fill up modem's buffer)
 tc class add dev ${INET} parent 1:1 classid 1:10 htb rate ${PHONE_RATE} ceil 300kbit prio 0
 
-#here comes packets marked as high prio network traffic (skype? starcraft, quake, outcoming ssh, packages with SYN)
+# here come packets marked as high prio network traffic (skype? starcraft, quake, outcoming ssh, packages with SYN)
 tc class add dev ${INET} parent 1:1 classid 1:11 htb rate ${HIGH_PRIO_RATE} ceil ${MAXUP} prio 1
 
-#here comes network
+# here comes network
 tc class add dev ${INET} parent 1:1 classid 1:12 htb rate ${NETWORK_RATE} ceil ${MAXUP} prio 2
 
-#here comes the rest - http, mail, and so on - services on infractus, and everything else marked as low priority
+# here comes the rest - http, mail, and so on - services on infractus, and everything else marked as low priority
 tc class add dev ${INET} parent 1:1 classid 1:13 htb rate ${SERVER_RATE} ceil ${MAXUP} prio 3
 
-#stochastic
-#obsluga stochastyczna
+# stochastic
 tc qdisc add dev ${INET} parent 1:11 handle 120: sfq perturb 10
 tc qdisc add dev ${INET} parent 1:12 handle 130: sfq perturb 10
 tc qdisc add dev ${INET} parent 1:13 handle 140: sfq perturb 10
 
 
-#setting filtering on IPTABLES rules
+# setting filtering on IPTABLES rules
 tc filter add dev ${INET} parent 1:0 protocol ip prio 1 handle 1 fw classid 1:10
 tc filter add dev ${INET} parent 1:0 protocol ip prio 2 handle 2 fw classid 1:11
 tc filter add dev ${INET} parent 1:0 protocol ip prio 3 handle 3 fw classid 1:12
 tc filter add dev ${INET} parent 1:0 protocol ip prio 4 handle 4 fw classid 1:13
 
 
-#firewall classification
+# firewall classification
+# phone
 iptables -t mangle -A PREROUTING -s 10.0.1.2 -j MARK --set-mark 0x1
 iptables -t mangle -A PREROUTING -s 10.0.1.2 -j RETURN
 
